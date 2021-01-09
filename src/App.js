dc12d8c93af45af13c0e216a32c3a3b225c7ead8
Ryoji Park <dlbia1106@gmail.com>
2021-01-08 20:02:41 -0800

    bitmobin player

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import browserImg from './browser-img.png';
+import cal from './calculation.png';
 
+import BitmovinPlayer from './bitmovinPlayer';
+import { detect } from 'detect-browser';
+const browser = detect();
 function App() {
+  const detectedCodecImg = () => {
+    if (browser.name === 'chrome' || browser.name === 'firefox') {
+      return 'V9'
+    } else if (browser.name === 'edge') {
+      return 'H265'
+    } else if (browser.name === 'safari') {
+      return 'H264'
+    }
+    return null 
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<>
+<div>
+    <div class="row">
+        <div class="col-lg-6 player-col mt-3">
+            <img src={browserImg}  />
+        </div>
+        <div class="col-lg-6 player-col mt-3">
+            <img src={cal}  />
+        </div>
     </div>
+    <div class="row mt-5">
+        <div class="col-lg-6 player-col">
+            <p>Detected <span id="detected-browser">{browser.name}</span>, using <span class="selected-codec">{detectedCodecImg()}</span></p>
+            <div className='content'>
+              <div id='player-wrapper'>
+                  <BitmovinPlayer />
+              </div>
+            </div>
+        </div>
+    </div>
+</div>
+</>
   );
 }
 
---
+++ src/bitmovinPlayer.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Player } from 'bitmovin-player';
+import { UIFactory } from 'bitmovin-player/bitmovinplayer-ui';
+import 'bitmovin-player/bitmovinplayer-ui.css';
+import { detect } from 'detect-browser';
+const browser = detect();
+class BitmovinPlayer extends React.Component {
+
+    state = {
+        player: null,
+    };
+
+    playerConfig = {
+        key: 'YOUR KEY HERE'
+    };
+
+    playerSource = () => {
+        const source = {
+            dash: '//bitmovin-a.akamaihd.net/webpages/demos/content/multi-codec/h264/stream.mpd',
+            poster: 'images/comparison.jpg'
+        };
+        if (browser.name === 'chrome' || browser.name === 'firefox') {
+            source.dash = '//bitmovin-a.akamaihd.net/webpages/demos/content/multi-codec/vp9/stream.mpd';
+        } else if (browser.name === 'edge') {
+            source.dash = '//bitmovin-a.akamaihd.net/webpages/demos/content/multi-codec/hevc/stream.mpd';
+        } else if (browser === 'safari') {
+            source.hls = '//bitmovin-a.akamaihd.net/webpages/demos/content/multi-codec/hevc/stream_fmp4.m3u8';
+        }
+        return source
+    }
+
+    constructor(props) {
+        super(props);
+        this.playerDiv = React.createRef();
+    }
+
+    componentDidMount() {
+        this.setupPlayer();
+    }
+
+    componentWillUnmount() {
+        this.destroyPlayer();
+    }
+
+    setupPlayer() {
+        const player = new Player(this.playerDiv.current, this.playerConfig);
+        UIFactory.buildDefaultUI(player);
+        player.load(this.playerSource()).then(() => {
+            this.setState({
+                ...this.state,
+                player
+            });
+            console.log('Successfully loaded source');
+        }, () => {
+            console.log('Error while loading source');
+        });
+    }
+
+    destroyPlayer() {
+        if (this.state.player != null) {
+            this.state.player.destroy();
+            this.setState({
+                ...this.state,
+                player: null
+            });
+        }
+    }
+
+    render() {
+        return <div id='player' ref={this.playerDiv}/>;
+    }
+}
+
+export default BitmovinPlayer;
